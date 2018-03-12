d94ed62393f4d5e1cd687b6dd12430c733d4fba5
Alcynos <33374011+Alcynos@users.noreply.github.com>
2018-03-12 18:46:16 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -49,6 +49,7 @@ function play() {
     }
 
     function isZero(number) {
+		// booléen vérifiant si number est 0
         if (number = 0) {
             return 1;
         } else {
@@ -57,6 +58,7 @@ function play() {
     }
 
     function isRed(number) {
+		// booléen vérifiant si la couleur de number est rouge
         if (!isZero(number)) {
             if (number = 2 || number = 4 || number = 6 || number = 8 || number = 10 || number = 11 || number = 13 || number = 15 || number = 17 || number = 20 || number = 22 || number = 24 || number = 26 || || number = 28 || number = 29 || || number = 31 || number = 33 || number = 36) {
                 return 0;
@@ -69,6 +71,7 @@ function play() {
     }
 
     function isEven(number) {
+		// booléen vérifiant si number est impair
         if (!isZero(number)) {
             if (number % 2 == 0) {
                 return 0;
@@ -79,6 +82,7 @@ function play() {
     }
 
     function moitie2(number) {
+		// booléen vérifiant si number est dans la seconde moitié
         if (!isZero(number)) {
             if (number > 0 && number < 19) {
                 return 0;
@@ -89,6 +93,7 @@ function play() {
     }
 
     function douzaine(number) {
+		// returne la douzaine-1 de number
         if (!isZero(number)) {
             if (number % 3 == 1) {
                 return 0;
@@ -99,7 +104,21 @@ function play() {
             }
         }
     }
+    
+    function result() {
+		number = Math.floor(Math.random() * 36)
+		// Censé tirer une valeur entre 0 et 36 (pas testé)
+		gain(number);
+	}
 
     function gain(number) {
-		//TODO
+		if (isZero(number) {
+			// TODO  parcours du tableau de mise en ignorant les number!=0
+		}
+		else {
+			/* TODO parcours du tableau de mise et si tab[i]=number retourner la valeur misée * 36
+			 *		si tab[i] affiche un bouton spécial (ex: pair), testé fonction(number)==fonction(tab[i])
+			 * 		si c'est true, retourner la valeur misée * x
+			*/
+		}
     }
