e45b5c94b1af80b37c67043f2891547d77606858
Alcynos <33374011+Alcynos@users.noreply.github.com>
2018-03-13 16:02:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ function play() {
 
 function isZero(number) {
 	// booléen vérifiant si number est 0
-    if (number = 0) {
+    if (number == 0) {
         return 1;
     } else {
         return 0;
@@ -63,67 +63,74 @@ function isZero(number) {
 }
 
 function isRed(number) {
-	// booléen vérifiant si la couleur de number est rouge
+	// vérifie la couleur de number
     if (!isZero(number)) {
         if ((number == 2) || (number == 4) || (number == 6) || (number == 8) || (number == 10) || (number == 11) || (number == 13) || (number == 15) || (number == 17) || (number == 20) || (number == 22) || (number == 24) || (number == 26) || (number == 28) || (number == 29) || (number == 31) || (number == 33) || (number == 36)) {
-            return 0;
+            return 42;
             // est noir
         } else {
-            return 1;
+            return 43;
             // est rouge
         }
     }
 }
 
     function isEven(number) {
-		// booléen vérifiant si number est impair
+		// vérifie si number est impair ou pair
         if (!isZero(number)) {
             if (number % 2 == 0) {
-                return 0;
+                return 41;
             } else {
-                return 1;
+                return 44;
             }
         }
     }
 
-    function moitie2(number) {
-		// booléen vérifiant si number est dans la seconde moitié
+    function moitie(number) {
+		// vérifie la moitié de number
         if (!isZero(number)) {
             if (number > 0 && number < 19) {
-                return 0;
+                return 40;
             } else {
-                return 1;
+                return 45;
             }
         }
     }
 
     function douzaine(number) {
-		// returne la douzaine-1 de number
+		// retourne la douzaine de number
         if (!isZero(number)) {
             if (number % 3 == 1) {
-                return 0;
+                return 37;
             } else if (number % 3 == 2) {
-                return 1;
+                return 38;
             } else {
-                return 2;
+                return 39;
             }
         }
     }
-    
-    function result() {
-		number = Math.floor(Math.random() * 36);
-		// Censé tirer une valeur entre 0 et 36 (pas testé)
-		gain(number);
-	}
 
     function gain(number) {
 		if (isZero(number)) {
-			// TODO  parcours du tableau de mise en ignorant les number!=0
+			for (int i = 0; i <= tableauMises.length; i++) {
+				if (isZero(tableauMises[i]) {
+					jeton = jeton + 36 * tableauValeur[i];
+				}
+			}
 		}
 		else {
-			/* TODO parcours du tableau de mise et si tab[i]=number retourner la valeur misée * 36
-			 *		si tab[i] affiche un bouton spécial (ex: pair), testé fonction(number)==fonction(tab[i])
-			 * 		si c'est true, retourner la valeur misée * x
-			*/
+			for (int i = 0; i <= tableauMises.length; i++) {
+				if (tableauMises[i] == isRed(number) || tableauMises[i] == isEven(number) || tableauMises[i] == moitie(number)) {
+					jeton = jeton + 2 * tableauValeur[i];
+				}
+				else if (tableauMises[i] == douzaine(number)) {
+					jeton = jeton + 3 * tableauValeur[i];
+				}
+				else if (tableauMises[i] == number && tableauMises[i]) {
+					jeton = jeton + 36 * tableauValeur[i];
+				}
 		}
     }
+    tableauMises = [];
+    tableauValeur = [];
+}
