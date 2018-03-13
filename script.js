1734e4b8f33844288ae2ff296bf0b026f61b16bd
BIGBISOUS <37078031+BIGBISOUS@users.noreply.github.com>
2018-03-13 11:48:57 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ function ajoutBet(param, valeurMise) {
         if (!verouiller) {
             pari.value = pari.value + '+' + champ.value;
             calculer();
-	    tableauMises.push(valeurMise);
+	        tableauMises.push(valeurMise);
             jeton = jeton - champ.value;
             displayJeton();
-	    gain.value = pari.value * param;
+	        gain.value = pari.value * param;
         }
     } else {
         pari.value = "Erreur sale pauvre";
@@ -51,28 +51,29 @@ function play() {
     for (var i = 0; i <= 36; i++) {
         playball.style.transform = rotate(9);
     }
+}
 
-    function isZero(number) {
-		// booléen vérifiant si number est 0
-        if (number = 0) {
-            return 1;
-        } else {
+function isZero(number) {
+	// booléen vérifiant si number est 0
+    if (number = 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+function isRed(number) {
+	// booléen vérifiant si la couleur de number est rouge
+    if (!isZero(number)) {
+        if ((number == 2) || (number == 4) || (number == 6) || (number == 8) || (number == 10) || (number == 11) || (number == 13) || (number == 15) || (number == 17) || (number == 20) || (number == 22) || (number == 24) || (number == 26) || (number == 28) || (number == 29) || (number == 31) || (number == 33) || (number == 36)) {
             return 0;
+            // est noir
+        } else {
+            return 1;
+            // est rouge
         }
     }
-
-    function isRed(number) {
-		// booléen vérifiant si la couleur de number est rouge
-        if (!isZero(number)) {
-            if (number = 2 || number = 4 || number = 6 || number = 8 || number = 10 || number = 11 || number = 13 || number = 15 || number = 17 || number = 20 || number = 22 || number = 24 || number = 26 || || number = 28 || number = 29 || || number = 31 || number = 33 || number = 36) {
-                return 0;
-                // est noir
-            } else {
-                return 1;
-                // est rouge
-            }
-        }
-    }
+}
 
     function isEven(number) {
 		// booléen vérifiant si number est impair
@@ -110,13 +111,13 @@ function play() {
     }
     
     function result() {
-		number = Math.floor(Math.random() * 36)
+		number = Math.floor(Math.random() * 36);
 		// Censé tirer une valeur entre 0 et 36 (pas testé)
 		gain(number);
 	}
 
     function gain(number) {
-		if (isZero(number) {
+		if (isZero(number)) {
 			// TODO  parcours du tableau de mise en ignorant les number!=0
 		}
 		else {
